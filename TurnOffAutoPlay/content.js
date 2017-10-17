30da8a3fd00ac5db4e12628fb9c8cabcedaed418
Raul <rchfe23@gmail.com>
2017-10-17 00:29:14 -0400

    toggle button when programmatically clicked

--- TurnOffAutoPlay/content.js
+++ TurnOffAutoPlay/content.js
@@ -5,12 +5,19 @@ var toggleButtonId = 'toggle';
 function setToggle(){
     console.log(consoleMessage, 'setToggle');
     var toggleButton = document.getElementById(toggleButtonId);
-    toggleButton.setAttribute('disabled', 'disabled');
+    toggleButton.click();
 }
 
 function process(event) {
     console.log(consoleMessage, 'process() fired by event=' + event.type);
-    setToggle();
+    if(event.type === 'DOMContentLoaded')
+        setToggle();
+    else{
+        if(event.type === 'yt-navigate-start' &&
+            document.getElementById(toggleButtonId) !== null){
+            console.log(consoleMessage, 'test')
+        }
+    }
 }
 
 
@@ -18,13 +25,13 @@ function process(event) {
 console.log(consoleMessage, 'starting');
 
 // ref.: https://stackoverflow.com/a/34100952
-window.addEventListener("spfdone", process); // old youtube design
-window.addEventListener("yt-navigate-start", process); // new youtube design
+window.addEventListener('spfdone', process); // old youtube design
+window.addEventListener('yt-navigate-start', process); // new youtube design
 
 // ref.: http://callmenick.com/post/check-if-everything-loaded-with-javascript
 var everythingLoaded = setInterval(function() {
   if (document.getElementById(toggleButtonId) !== null) {
     clearInterval(everythingLoaded);
-    process({type: "DOMContentLoaded"}); // this is the function that gets called when everything is loaded
+    process({type: 'DOMContentLoaded'}); // this is the function that gets called when everything is loaded
   }
 }, 10);
