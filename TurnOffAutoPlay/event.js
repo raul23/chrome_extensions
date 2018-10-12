cb9fb6194fa2427fc6fa60495b75c259bb333c47
Raul <rchfe23@gmail.com>
2018-10-11 21:58:08 -0400

    add comments

--- TurnOffAutoPlay/event.js
+++ TurnOffAutoPlay/event.js
@@ -3,7 +3,10 @@ console.log("It's a me, event.js!");
 
 chrome.runtime.onInstalled.addListener(
     function(details) {
-        // Using console.log() we won't see the messages, use alert() instead
+        // Using console.log() we won't see the messages on the Inspect's
+        // Console tab, use alert() instead. However, you can still see the
+        // console.log's messages by going to the extension's page and clicking
+        // on "Inspect views->background page"
         console.log(consoleMessage + "chrome.runtime.onInstalled event");
         // alert(consoleMessage + "chrome.runtime.onInstalled event");
     }
